05d779f0a2828c305f4d2294aa5ba224daf954f1
sgw <1226189603@qq.com>
2020-08-28 16:46:31 +0800

    章节

--- src/components/Programinfo.vue
+++ src/components/Programinfo.vue
@@ -2,10 +2,46 @@
     <div>
 
       <el-drawer
+        title="章节目录"
+        :visible.sync="chapter"
+        :direction="direction"
+        :before-close="handleClose"
+        size="70%">
+        <el-table :data="chapters">
+          <el-table-column prop="ctid" label="编号"></el-table-column>
+          <el-table-column prop="title" label="标题"></el-table-column>
+          <el-table-column prop="artist" label="作者"></el-table-column>
+          <el-table-column prop="src" label="音频路径"></el-table-column>
+          <el-table-column prop="state" label="是否付费">
+            <template slot-scope="scope">
+              {{scope.row.state==0?'免费':'付费'}}
+            </template>
+          </el-table-column>
+          <el-table-column prop="virtualcurrency" label="应付喜币"></el-table-column>
+
+          <el-table-column prop="createdate" label="上传时间"></el-table-column>
+
+        </el-table>
+        <div class="block">
+          <el-pagination
+            @size-change="handleSize"
+            @current-change="handleCurrent"
+            :current-page="currentPage"
+            :page-sizes="[5, 10, 20]"
+            :page-size="pagesize"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="total">
+          </el-pagination>
+        </div>
+
+      </el-drawer>
+
+      <el-drawer
         title="详细信息"
         :visible.sync="drawer"
         :direction="direction"
-        :before-close="handleClose">
+        :before-close="handleClose"
+         size="50%">
         <div>
           <!--详情页面-->
           <el-table :data="details">
@@ -50,6 +86,11 @@
             <el-button icon="el-icon-search"  @click="querydetails(scope.row)" circle></el-button>
           </template>
         </el-table-column>
+        <el-table-column label="章节目录">
+          <template slot-scope="scope">
+            <el-button icon="el-icon-search"  @click="querychapters(scope.row)" circle></el-button>
+          </template>
+        </el-table-column>
         <el-table-column   prop="pstatus" label="上下架">
           <template slot-scope="scope">
             <el-switch
@@ -58,15 +99,11 @@
               :active-value="0"
               inactive-text="下架"
               :inactive-value="1"
-              @change="changePro($event,scope.row)">
+              @change="changeStatus($event,scope.row)">
             </el-switch>
           </template>
         </el-table-column>
-        <el-table-column  label="操作" fixed="right" width="70px">
-          <template slot-scope="scope">
-            <el-button type="text" @click="showByrow(scope.row)">修改</el-button>
-          </template>
-        </el-table-column>
+
       </el-table>
 
       <div class="block">
@@ -93,34 +130,57 @@
             pagesize: 5, //每页的数据条数
             currentPage: 1, //默认
             drawer:false,
-            direction: 'btt',
+            chapter:false,
+            direction: 'rtl',//设置默认
             details:[],
+            chapters:[],
+
           }
         },created:function(){
           this.queryList();
       },methods:{
           queryList:function () {
+            console.log("主"+this.currentPage)
+            console.log("主"+this.pagesize)
             this.$axios.post("backstage/programinfo/programinfoAll",
               {"currentPage":this.currentPage,"pageSize":this.pagesize}).then(resp=>{
+                this.total=resp.data.total;
                 this.list=resp.data.list;
             })
           },
         handleSizeChange(size) {
-          console.log(`每页 ${size} 条`);
           this.pagesize=size;
           this.queryList();
         },
         handleCurrentChange(curPage) {
-          console.log(`当前页: ${curPage}`);
           this.currentPage=curPage;
           this.queryList();
         },
         querydetails:function (row) {
           this.drawer=true;
+          this.direction= 'btt',
           this.$axios.post("backstage/programinfo/programinfoAll",
             {"currentPage":this.currentPage,"pageSize":this.pagesize,"pid":row.pid}).then(resp=>{
              this.details=resp.data.list;
           })
+        },
+        //章节目录
+        querychapters:function(row){
+          this.chapter=true;
+          this.direction= 'rtl'
+         this.$axios.post("backstage/chapterinfo/chapterinfoQuery",
+            {"currentPage":this.currentPage,"pageSize":this.pagesize,"pid":row.pid}).then(resp=>{
+              console.log(resp.data.list);
+              this.total=resp.data.total;
+              this.chapters=resp.data.list;
+          })
+        },handleSize:function(){
+          this.pagesize=size;
+          this.querychapters();
+        },
+        handleCurrent:function(){
+          this.currentPage=curPage;
+          this.querychapters();
         },
         handleClose(done) {
           this.$confirm('确认关闭？')
@@ -128,6 +188,17 @@
               done();
             })
             .catch(_ => {});
+        },
+        changeStatus:function (val, row) {
+          this.$axios.post("backstage/programinfo/updateppstatus"
+            ,{"pstatus":row.pstatus,"pid":row.pid}).then(response=>{
+              if(response.data==1){
+                this.$message({
+                  type: 'success',
+                  message: '操作成功!'
+                })
+              }
+          })
         }
       }
     }
